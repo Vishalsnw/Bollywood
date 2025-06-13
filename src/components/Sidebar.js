2bbca2d278f03df5a73d3a44db4a4b6eede6b347
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-14 01:56:22 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Movie as MovieIcon, Star as StarIcon, AttachMoney as MoneyIcon } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 const Sidebar = ({ open, toggleSidebar }) => {
   return (
     <Drawer anchor="left" open={open} onClose={toggleSidebar}>
       <List>
-        <ListItem button>
+        <ListItem button component={Link} to="/producers">
           <ListItemIcon>
             <MovieIcon />
           </ListItemIcon>
           <ListItemText primary="Producers" />
         </ListItem>
-        <ListItem button>
+        <ListItem button component={Link} to="/news">
           <ListItemIcon>
             <StarIcon />
           </ListItemIcon>
-          <ListItemText primary="Oscar Nominations" />
+          <ListItemText primary="News" />
         </ListItem>
-        <ListItem button>
+        <ListItem button component={Link} to="/oscars/winners">
           <ListItemIcon>
             <MoneyIcon />
           </ListItemIcon>
