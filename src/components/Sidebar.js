aa77bbaae1a247038dc5db55e93f647f71bed1a6
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2025-06-14 13:23:57 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,50 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { Movie as MovieIcon, Star as StarIcon, AttachMoney as MoneyIcon } from '@mui/icons-material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Box, Typography } from '@mui/material';
+import { Movie as MovieIcon, Star as StarIcon, AttachMoney as MoneyIcon, Home as HomeIcon } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
 const Sidebar = ({ open, toggleSidebar }) => {
   return (
-    <Drawer anchor="left" open={open} onClose={toggleSidebar}>
+    <Drawer 
+      anchor="left" 
+      open={open} 
+      onClose={toggleSidebar}
+      sx={{
+        width: 240,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: 240,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h6" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
+          Bollywood Game
+        </Typography>
+      </Box>
       <List>
-        <ListItem button component={Link} to="/producers">
+        <ListItem button component={Link} to="/" onClick={toggleSidebar}>
           <ListItemIcon>
-            <MovieIcon />
+            <HomeIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText primary="Game Home" />
+        </ListItem>
+        <ListItem button component={Link} to="/producers" onClick={toggleSidebar}>
+          <ListItemIcon>
+            <MovieIcon color="primary" />
           </ListItemIcon>
           <ListItemText primary="Producers" />
         </ListItem>
-        <ListItem button component={Link} to="/news">
+        <ListItem button component={Link} to="/news" onClick={toggleSidebar}>
           <ListItemIcon>
-            <StarIcon />
+            <StarIcon color="primary" />
           </ListItemIcon>
           <ListItemText primary="News" />
         </ListItem>
-        <ListItem button component={Link} to="/oscars/winners">
+        <ListItem button component={Link} to="/oscars/winners" onClick={toggleSidebar}>
           <ListItemIcon>
-            <MoneyIcon />
+            <MoneyIcon color="primary" />
           </ListItemIcon>
           <ListItemText primary="Oscar Winners" />
         </ListItem>
